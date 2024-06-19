485f0c84382e54fe66a9ff79266b9cd2a2be9914
krouu <jueunheo0317@gmail.com>
2024-06-19 20:17:09 +0900

    로그인/회원가입

---
+++ src/main/java/com/exampl/traveler/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.exampl.traveler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/header")
+    public String Header(Model model) {
+        return "header";
+    }
+    @GetMapping("/footer")
+    public String Footer(Model model){
+        return "foter";
+    }
+    @RequestMapping("/")
+    public String main(Model model){
+        return "/main/main";
+    }
+
+
+}
