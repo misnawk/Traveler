da629e43946ab7f70814c61f408ee8e826848f55
krouu <jueunheo0317@gmail.com>
2024-06-19 20:15:52 +0900

    로그인/회원가입

---
+++ src/main/java/com/exampl/traveler/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.exampl.traveler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "/login/login";
+    }
+
+    @RequestMapping("/binLogin")
+    public String binLogin(){
+        return "/login/binLogin";
+    }
+
+    @RequestMapping("/signUP")
+    public String signUp(){
+        return "/login/signUp";
+    }
+
+    @RequestMapping("/binSignUp")
+    public String binSignUp(){
+        return "/login/binSignUp";
+    }
+
+}
