5874ae63443a0fb1c2c0ea991a65707988f2875c
hayu6 <hayu6944@gmail.com>
2024-07-03 16:16:30 +0900

    호텔 결제 전까지 기능 구현

--- src/main/java/com/exampl/traveler/controller/HotelController.java
+++ src/main/java/com/exampl/traveler/controller/HotelController.java
@@ -1,14 +1,19 @@
 package com.exampl.traveler.controller;
 
 import com.exampl.traveler.service.HotelService;
+import com.exampl.traveler.vo.HotelVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HotelController {
-
 
     @Autowired
     private HotelService hotelService;
@@ -19,4 +24,22 @@ public class HotelController {
         return "hotel/hotelMain";
     }
 
+    @GetMapping("/hotel/place/{place}")
+    public String getHotelsByPlace(@PathVariable("place") String place, Model model) {
+        List<HotelVO> hotels = hotelService.getHotelsByPlace(place);
+        model.addAttribute("hotels", hotels);
+        return "hotel/hotelPlace";
+    }
+
+    @GetMapping("/hotel/hotelDetail/{hotelNO}")
+    public String getHotelDetail(@PathVariable("hotelNO") String hotelNO, Model model) {
+        HotelVO hotel = hotelService.getHotelById(hotelNO);
+        model.addAttribute("hotel", hotel);
+        return "hotel/hotelDetail";
+    }
+
+    @GetMapping("/hotel/payment")
+    public String showPaymentPage() {
+        return "hotel/hotelPayment";
+    }
 }
--- src/main/java/com/exampl/traveler/service/HotelService.java
+++ src/main/java/com/exampl/traveler/service/HotelService.java
@@ -3,6 +3,7 @@ package com.exampl.traveler.service;
 import com.exampl.traveler.mapper.HotelMapper;
 import com.exampl.traveler.vo.HotelVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,4 +17,13 @@ public class HotelService {
     public List<HotelVO> getAllHotels() {
         return hotelMapper.getAllHotels();
     }
+
+    public List<HotelVO> getHotelsByPlace(String place){
+        return hotelMapper.getHotelsByPlace(place);
+    }
+
+    public HotelVO getHotelById(String hotelNO) {
+        return hotelMapper.getHotelById(hotelNO);
+    }
+
 }
