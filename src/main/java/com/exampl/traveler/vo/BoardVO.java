7ea26a963ba036197d148a965e68d45c21131a10
leeuihyoun <dasuu3@naver.com>
2024-07-03 18:50:48 +0900

    게시판 작성과 상세보기 적용 완료

--- src/main/java/com/exampl/traveler/vo/BoardVO.java
+++ src/main/java/com/exampl/traveler/vo/BoardVO.java
@@ -1,6 +1,5 @@
 package com.exampl.traveler.vo;
 
-import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
