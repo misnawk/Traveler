df124aaabcb079351e315e3c6f8a4635539672d8
krouu <jueunheo0317@gmail.com>
2024-07-12 20:00:10 +0900

    sign up css 수정

--- src/main/resources/static/js/login/binSignUp.js
+++ src/main/resources/static/js/login/binSignUp.js
@@ -89,8 +89,8 @@ function save(){
     let $sendBtn = $("input#singUp");
 
     if(idSccess == false){
-        idMsg.style.color = red;
-        idMsg.innerHTML = "아이디 중복 확인을 해주세요"
+//        idMsg.style.color = red;
+//        idMsg.innerHTML = "아이디 중복 확인을 해주세요"
         document.getElementById("binID").value = "";
         return;
     }
